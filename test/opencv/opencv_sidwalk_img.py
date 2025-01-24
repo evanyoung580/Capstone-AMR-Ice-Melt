9e6dddd984962e7accaca22c509b72591d472a11
evan young <emy1212@rit.edu>
2025-01-23 23:12:46 -0500

    position calculation for edges

--- test/opencv/opencv_sidwalk_img.py
+++ test/opencv/opencv_sidwalk_img.py
@@ -5,20 +5,24 @@ def make_line_length(longest):
     def line_length(line):
         x1, y1, x2, y2 = line[0] 
         angle = np.arctan2(y2 - y1, x2 - x1) * (180 / np.pi)
-        #print(angle)
-        if ((angle > 30) and (angle < 150)) or ((angle < -30) and (angle > -150)):
+        ang_excl = config['image_proc']['line_detect']['line']['ang_excl']
+        if ((angle > ang_excl) and (angle < 180 - ang_excl)) or \
+            ((angle < -ang_excl) and (angle > -(180 - ang_excl))):
             if longest is not None:
                 x1ll, _, x2ll, _ = longest[0]
-                dist = 50
-                x2d = abs(x2-x2ll) > dist
-                x1d = abs(x1-x1ll) > dist
+                x_gap = config['image_proc']['line_detect']['line']['x_gap']
+                x2d = abs(x2-x2ll) > x_gap
+                x1d = abs(x1-x1ll) > x_gap
                 if (x2d and x1d):
                     length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
                 else:
                     length = 0
             else:
                 length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-            return length
+        else: 
+            length = 0
+        return length
+        
     return line_length
         
 
@@ -47,18 +51,43 @@ resized_image = cv2.resize(cropped_image, (width * k_rescale, height * k_rescale
 
 gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 blur_image = cv2.blur(gray_image, (5, 5))
-edge_image = cv2.Canny(blur_image, 100, 150)
+edge_image = cv2.Canny(blur_image, config['image_proc']['line_detect']['canny']['low'], 
+                       config['image_proc']['line_detect']['canny']['low'])
 lines = cv2.HoughLinesP(edge_image, 1, np.pi / 180,
-                         50, minLineLength=50, maxLineGap=150)
+                        config['image_proc']['line_detect']['hough']['thresh'], 
+                        minLineLength=config['image_proc']['line_detect']['hough']['min_len'], 
+                        maxLineGap=config['image_proc']['line_detect']['hough']['max_gap'])
 if lines is not None:
     longest = make_line_length(None)
     long_line = max(lines, key=longest)
     sec_longest = make_line_length(long_line)
     sec_long_line = max(lines, key=sec_longest)
-    x1ll, y1ll, x2ll, y2ll = long_line[0]
-    x1sl, y1sl, x2sl, y2sl = sec_long_line[0]
+    long_line_len = longest(long_line)
+    sec_long_line_len = longest(sec_long_line)
+    if (long_line_len > 0) and (sec_long_line_len > 0):
+        x1ll, y1ll, x2ll, y2ll = long_line[0]
+        x1sl, y1sl, x2sl, y2sl = sec_long_line[0]
+        if x1ll < x1sl:
+            left_line = long_line
+            right_line = sec_long_line
+            x1ll, y1ll, x2ll, y2ll = left_line[0]
+            x1rl, y1rl, x2rl, y2rl = right_line[0]
+        else:
+            left_line = sec_long_line
+            right_line = long_line
+            x1ll, y1ll, x2ll, y2ll = left_line[0]
+            x1rl, y1rl, x2rl, y2rl = right_line[0]
+        left_edge = ((np.average([x1ll, x2ll]) / width)) - 1
+        right_edge = ((np.average([x1rl, x2rl]) / width)) - 1
+        center = np.average([left_edge, right_edge])
+
+        print(f"Left edge: {left_edge}, Right edge: {right_edge}, Center: {center}")
+            
+    if sec_long_line_len > 0:
+        x1sl, y1sl, x2sl, y2sl = sec_long_line[0]
+    
     cv2.line(resized_image, (x1ll, y1ll), (x2ll, y2ll), (0, 0, 255), 4)
-    cv2.line(resized_image, (x1sl, y1sl), (x2sl, y2sl), (255, 0, 0), 4)
+    cv2.line(resized_image, (x1rl, y1rl), (x2rl, y2rl), (255, 0, 0), 4)
 
 cv2.imshow("original", image)
 cv2.imshow("cropped", resized_image)
